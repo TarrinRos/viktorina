67557a0cee5f53d0bdfe0a03cae13c1dcda9455a
TarrinRos <tarindis@gmail.com>
2018-10-17 13:16:25 +0300

    Add question.xml

--- lib/question.rb
+++ lib/question.rb
@@ -1,5 +1,14 @@
 class Question
-  def initialize
+  def self.from_file(questions_path)
+    question =  File.readlines(questions_path, chomp: true)
+    self.new(question)
+  end
 
+  def initialize(question)
+    @question = question[0]
+  end
+
+  def to_s
+    "#{@question}"
   end
 end
